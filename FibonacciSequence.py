a7f9c1b49956bbd26ef23cccc740f11246c35c99
Rohan Pankaj <rohansp01@gmail.com>
2019-02-06 23:34:01 -0800

    Added documentation/comments

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -1,12 +1,12 @@
-def create_sequence(a,b,c):
+def create_sequence(a,b,c): #make Function
 
-    while a > 0: 
-        c = a+b
+    while a > 0: #while loop created with the conditional that it should run when a is greater than zero (should always be true with given values)
+        c = a+b 
         print(c)
         b = c+a
         print(b)
         a = b+c
         print(a)
 
-create_sequence(1,0,0)
+create_sequence(1,0,0) #setting values
 
