4498018414f7a110922b0fcce3e97250655545c2
Rohan Pankaj <rohansp01@gmail.com>
2019-02-07 11:48:04 -0800

    finding the nth value

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -1,14 +1,15 @@
-def create_sequence(a,b,c): #make Function
-
-    while a > 0: #while loop created with the conditional that it should run when a is greater than zero (should always be true with given values)
-        c = a+b 
-        print(c)
-        b = c+a
-        print(b)
-        a = b+c
-        print(a)
-
-create_sequence(1,0,0) #setting values
+def create_sequence(a,b,c,input,test): #make Function
+    while test < (input + 1): #while loop created with the conditional that it should run when a is greater than zero (should always be true with given values)
+        if input == test:
+                print(test)
+        else:
+                c = a+b 
+                test = c
+                b = c+a
+                test = b
+                a = b+c
+                test = a
+create_sequence(1,0,0,8,0) #setting values
 
 # Copyrite © Rohan S. Pankaj
 
---
+++ new test.py
@@ -0,0 +1,5 @@
+def fib_seq(a,b,c):
+    while a==b+c:
+
+fib_seq(b-c,3,9):
+
